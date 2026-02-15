c82140bfc607ca051fe36b1a7e87637851fcedb2
Votline <quogg2@gmail.com>
2026-02-15 13:25:15 +0300

    feat: add websocket connect, multi-user support and improved frontend
    - Backend Changes:
      * Use gorilla/websocket for update connection to websocket
      * Save connections in map
      * Send messages to all clients
      * Add LastOffer caching
    - Frontend changes:
      * Add unique peer ID (myID) for multiple simultaneous viewers
      * Add roles for clients (sender and viewer)
      * Add button to comply with browser autoplay policies

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"sync"
 
-	"go.uber.org/zap"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"go.uber.org/zap"
+)
+
+var (
+	upg   = &websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
+	conns = make(map[*websocket.Conn]bool)
+	lastOffer []byte
+	mu    sync.Mutex
 )
 
 func main() {
@@ -13,12 +22,68 @@ func main() {
 	if err != nil { panic(err) }
 	r := gin.Default()
 
-	r.GET("/hello", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, "Hello world")
+	upg = &websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	conns = make(map[*websocket.Conn]bool)
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.File("frontend/index.html")
 	})
 
-	fmt.Print("started")
-	if err := r.Run(":8080"); err != nil {
+	r.GET("/ws", func(ctx *gin.Context) {
+		conn, err := upg.Upgrade(ctx.Writer, ctx.Request, nil)
+		if err != nil {
+			l.Error("WS upgrade failed", zap.Error(err))
+			http.Error(ctx.Writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer conn.Close()
+
+		mu.Lock()
+		conns[conn] = true
+		if lastOffer != nil && !ctx.Request.URL.Query().Has("stream") {
+			conn.WriteMessage(websocket.TextMessage, lastOffer)
+		}
+		mu.Unlock()
+
+		defer func(){
+			mu.Lock()
+			delete(conns, conn)
+			mu.Unlock()
+			conn.Close()
+		}()
+
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				l.Error("Read message failed", zap.Error(err))
+				break
+			}
+
+			var data map[string]any
+			if err := json.Unmarshal(msg, &data); err != nil {
+				if _, has := data["sdp"]; has {
+					mu.Lock()
+					lastOffer = msg
+					mu.Unlock()
+				}
+			}
+
+			mu.Lock()
+			for c := range conns {
+				if c != conn {
+					c.WriteMessage(websocket.TextMessage, msg)
+				}
+			}
+			mu.Unlock()
+		}
+	})
+
+	if err := r.Run("0.0.0.0:8080"); err != nil {
 		l.Panic("Server run failed", zap.Error(err))
 	}
 }
